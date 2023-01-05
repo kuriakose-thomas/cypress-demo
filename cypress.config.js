d636caf9848490d448f4654b7e029fe081c222fb
Kuriakose Thomas <103944398+kuriakose-thomas@users.noreply.github.com>
2023-01-04 23:13:34 -0500

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  projectId: 'qrsixy',
+  projectId: 'zuonih',
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
